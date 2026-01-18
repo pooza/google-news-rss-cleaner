44acb72c4ff82fe1b12939aabc1432024876f9ad
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2026-01-18 22:29:12 +0900

    add: eslint

---
+++ eslint.config.cjs
@@ -0,0 +1,16 @@
+module.exports = [
+  {
+    ignores: ['node_modules/', 'dist/'],
+  },
+  {
+    files: ['**/*.js', '**/*.cjs'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+    },
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+    },
+  },
+];
